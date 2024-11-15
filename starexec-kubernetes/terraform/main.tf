0e6466372bf46d18f4097b921229ab8d8526dc78
Geoff Sutcliffe <2geoff@pobox.com>
2024-11-15 19:21:06 +0000

    README

--- starexec-kubernetes/terraform/main.tf
+++ starexec-kubernetes/terraform/main.tf
@@ -75,11 +75,11 @@ data "external" "instance_type" {
 
 #################### EKS ####################################################
 
-data "aws_eks_cluster_versions" "available" {}
+# data "aws_eks_cluster_versions" "available" {}
 
-locals {
-  latest_k8s_version = versionmax(data.aws_eks_cluster_versions.available.versions)
-}
+# locals {
+#   latest_k8s_version = versionmax(data.aws_eks_cluster_versions.available.versions)
+# }
 
 
 module "eks" {
@@ -87,8 +87,8 @@ module "eks" {
   version = "20.8.5"
 
   cluster_name    = local.cluster_name
-  # cluster_version = "1.30"
-  cluster_version = local.latest_k8s_version
+  cluster_version = "1.31"
+  # cluster_version = local.latest_k8s_version
 
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
