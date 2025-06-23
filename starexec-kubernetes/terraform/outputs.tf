306deaf538734010c8c038e0d974466d4ad19b49
Andres Caicedo <andrezz1997@gmail.com>
2025-06-23 02:13:04 -0500

    feat(outputs): add kubernetes_service data source for starexec-service

--- starexec-kubernetes/terraform/outputs.tf
+++ starexec-kubernetes/terraform/outputs.tf
@@ -1,6 +1,13 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+data "kubernetes_service" "starexec_service" {
+  metadata {
+    name      = "starexec-service"
+    namespace = "default"
+  }
+}
+
 output "cluster_endpoint" {
   description = "Endpoint for EKS control plane"
   value       = module.eks.cluster_endpoint
