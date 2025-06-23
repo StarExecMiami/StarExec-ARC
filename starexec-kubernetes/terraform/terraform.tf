79e307907286333c82b3baa35a371f245e0401ae
Andres Caicedo <andrezz1997@gmail.com>
2025-06-22 20:00:28 -0500

    fix(terraform): update S3 bucket name for backend configuration

--- starexec-kubernetes/terraform/terraform.tf
+++ starexec-kubernetes/terraform/terraform.tf
@@ -4,7 +4,7 @@
 terraform {
 
   backend "s3" {
-    bucket         = "starexec-kubernetes-terraform-state-cw7u822f"
+    bucket         = "starexec-kubernetes-terraform-state-kxiu6ah2"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "starexec-kubernetes-terraform-locks"
