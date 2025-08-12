948b8b9be4f611501a20fad25ba95286c59cd705
Andres Caicedo <andrezz1997@gmail.com>
2025-08-12 11:18:40 -0500

    refactor: modularize VPC variables and outputs
    Separates VPC-related variables and outputs into dedicated files for improved modularity and maintainability. Updates VPC module configuration with additional options like enabling public IP mapping and creating an internet gateway. Simplifies main file structure by removing inline variables and outputs.

--- starexec-kubernetes/terraform/modules/vpc/main.tf
+++ starexec-kubernetes/terraform/modules/vpc/main.tf
@@ -1,53 +1,6 @@
 # VPC Module
 # Responsible for creating the VPC, subnets, and related networking components
 
-variable "vpc_name" {
-  description = "Name of the VPC"
-  type        = string
-  default     = "starexec-vpc"
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "azs" {
-  description = "Availability zones for the VPC"
-  type        = list(string)
-}
-
-variable "private_subnets" {
-  description = "CIDR blocks for private subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
-variable "public_subnets" {
-  description = "CIDR blocks for public subnets"
-  type        = list(string)
-  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-}
-
-variable "enable_nat_gateway" {
-  description = "Whether to enable NAT gateway"
-  type        = bool
-  default     = true
-}
-
-variable "single_nat_gateway" {
-  description = "Whether to use a single NAT gateway"
-  type        = bool
-  default     = true
-}
-
-variable "enable_dns_hostnames" {
-  description = "Whether to enable DNS hostnames"
-  type        = bool
-  default     = true
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.8.1"
@@ -63,6 +16,16 @@ module "vpc" {
   single_nat_gateway   = var.single_nat_gateway
   enable_dns_hostnames = var.enable_dns_hostnames
 
+  map_public_ip_on_launch = true
+
+  create_igw = true
+  enable_vpn_gateway = false
+
+  tags = {
+    "Name" = var.vpc_name
+  }
+  
+
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
   }
@@ -70,24 +33,4 @@ module "vpc" {
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
   }
-}
-
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = module.vpc.vpc_id
-}
-
-output "private_subnets" {
-  description = "List of IDs of private subnets"
-  value       = module.vpc.private_subnets
-}
-
-output "public_subnets" {
-  description = "List of IDs of public subnets"
-  value       = module.vpc.public_subnets
-}
-
-output "vpc_cidr_block" {
-  description = "The CIDR block of the VPC"
-  value       = module.vpc.vpc_cidr_block
 }
---
+++ starexec-kubernetes/terraform/modules/vpc/variable.tf
@@ -0,0 +1,47 @@
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "starexec-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "azs" {
+  description = "Availability zones for the VPC"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "public_subnets" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "enable_nat_gateway" {
+  description = "Whether to enable NAT gateway"
+  type        = bool
+  default     = true
+}
+
+variable "single_nat_gateway" {
+  description = "Whether to use a single NAT gateway"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_hostnames" {
+  description = "Whether to enable DNS hostnames"
+  type        = bool
+  default     = true
+}
